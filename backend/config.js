b0b02dd4322f7b34f3b83ddc704cae5f7f7a935d
Miguel Angel Diego <madhosoi@gmail.com>
2018-09-19 17:46:02 +0200

    Begginer's Guide to APIs demo app

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   serverPort: process.env.PORT || 3000,
-  mongodb: { url: "mongodb://mongo/cnets_chat" }
+  mongodb: { url: process.env.MONGO_URL || "mongodb://mongo/cns_chat" }
 };
